1670b08c23bd860047efe2284a08a434afaadd24
Xebarter <sebenock027@gmail.com>
2025-01-18 19:11:15 +0300

    changed bcrypt to bcryptjs in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
