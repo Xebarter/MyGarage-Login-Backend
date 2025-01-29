fb430d1deb048be6782b37f21f9230d461ac10e5
Xebarter <sebenock027@gmail.com>
2025-01-30 01:57:17 +0300

    corrects access to server

--- server.js
+++ server.js
@@ -84,6 +84,10 @@ app.post('/login', async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('Backend is running successfully!');
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
